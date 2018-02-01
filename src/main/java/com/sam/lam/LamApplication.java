8c3d98023fd5e0d7335cd081dd643c42461077a9
Sam <samlamgithub@users.noreply.github.com>
2018-02-01 23:02:52 +0000

    init project

---
+++ src/main/java/com/sam/lam/LamApplication.java
@@ -0,0 +1,12 @@
+package com.sam.lam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LamApplication.class, args);
+	}
+}
